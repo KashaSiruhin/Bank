8423921eafb8c76550e2940386a33fad639da1fc
Kasha Sirukhin <13211123az@gmail.com>
2023-11-17 23:54:12 +0300

    Add custom inputs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,10 @@ class HomeController < ApplicationController
   protect_from_forgery prepend: true
 
   def index
+    if user_signed_in?
+      redirect_to edit_user_registration_path
+    else
+      redirect_to new_user_registration_path
+    end
   end
 end
